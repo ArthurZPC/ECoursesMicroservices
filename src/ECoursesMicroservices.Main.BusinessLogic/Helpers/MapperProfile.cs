e101748252879d2e9821d98899fb2e4f3a66133f
ArthurZPC <guseynoff.artur@yandex.ru>
2024-11-22 04:01:22 +0300

    - initial command mapper profile updates

--- src/ECoursesMicroservices.Main.BusinessLogic/Helpers/MapperProfile.cs
+++ src/ECoursesMicroservices.Main.BusinessLogic/Helpers/MapperProfile.cs
@@ -3,12 +3,22 @@ using ECoursesMicroservices.Main.BusinessLogic.DTOs.Authors;
 using ECoursesMicroservices.Main.BusinessLogic.DTOs.Categories;
 using ECoursesMicroservices.Main.BusinessLogic.DTOs.Courses;
 using ECoursesMicroservices.Main.BusinessLogic.DTOs.Tags;
+using ECoursesMicroservices.Main.BusinessLogic.Features.Authors.Commands;
+using ECoursesMicroservices.Main.BusinessLogic.Features.Categories.Commands;
+using ECoursesMicroservices.Main.BusinessLogic.Features.Courses.Commands;
+using ECoursesMicroservices.Main.BusinessLogic.Features.Tags.Commands;
 using ECoursesMicroservices.Main.Data.Entities;
 
 namespace ECoursesMicroservices.Main.BusinessLogic.Helpers;
 public class MapperProfile : Profile
 {
     public MapperProfile()
+    {
+        ConfigureDtoMapping();
+        ConfigureCommandsMapping();
+    }
+
+    public void ConfigureDtoMapping()
     {
         CreateMap<TagDto, Tag>()
             .ReverseMap();
@@ -21,5 +31,31 @@ public class MapperProfile : Profile
 
         CreateMap<CourseDto, Course>()
             .ReverseMap();
+    }
+
+    private void ConfigureCommandsMapping()
+    {
+        CreateMap<CreateAuthorCommand, Author>();
+
+        CreateMap<CreateCategoryCommand, Category>();
+
+        CreateMap<CreateCourseCommand, Course>()
+            .ForMember(c => c.Tags, opt => opt.Ignore());
+
+        CreateMap<CreateTagCommand, Tag>();
+
+        CreateMap<UpdateAuthorCommand, Author>()
+            .ForMember(x => x.Id, opt => opt.Ignore());
+
+        CreateMap<UpdateCategoryCommand, Category>()
+            .ForMember(x => x.Id, opt => opt.Ignore());
+
+        CreateMap<UpdateCourseCommand, Course>()
+            .ForMember(x => x.Id, opt => opt.Ignore())
+            .ForMember(x => x.Tags, opt => opt.Ignore());
+
+        CreateMap<UpdateTagCommand, Tag>()
+            .ForMember(x => x.Id, opt => opt.Ignore());
+
     }
 }
